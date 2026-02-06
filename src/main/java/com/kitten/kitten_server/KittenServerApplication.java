603434688cbb7eda8f780134d7907eb88be09aeb
pierre <pro.pierre.vidal@gmail.com>
2026-02-06 10:44:13 +0100

    Initial Spring Boot setup

---
+++ src/main/java/com/kitten/kitten_server/KittenServerApplication.java
@@ -0,0 +1,13 @@
+package com.kitten.kitten_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittenServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kitten/kitten_server/KittenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitten.kitten_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
